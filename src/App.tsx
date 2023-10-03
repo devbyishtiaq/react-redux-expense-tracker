653395290ee0ff2aa5370f7f2d6db217759b5130
devbyishtiaq <ishtiaaq.mushtaaq@gmail.com>
2023-10-03 12:57:12 +0500

    designed form list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import ExpenseList from "./components/ExpenseList";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container mx-auto min-h-screen overflow-hidden p-8">
+      <h1 className="text-3xl font-bold tracking-wider text-center">
+        Expense Tracker
+      </h1>
+      <div className="grid grid-cols-2 gap-8 mt-16">
+        <div>
+          <ExpenseList expenses={expenses} />
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    </div>
+  );
+};
 
-export default App
+export default App;
+
+const expenses = [
+  { id: 1, description: "hello world", amount: 100, category: "Utilities" },
+  { id: 2, description: "hello world", amount: 100, category: "Utilities" },
+  { id: 3, description: "hello world", amount: 100, category: "Utilities" },
+];
---
+++ src/components/ExpenseList.tsx
@@ -0,0 +1,83 @@
+interface Expense {
+  id: number;
+  description: string;
+  amount: number;
+  category: string;
+}
+
+interface Props {
+  expenses: Expense[];
+}
+
+const ExpenseList = ({ expenses }: Props) => {
+  return (
+    <div className="mt-8 flow-root">
+      <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+        <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+          <div className="overflow-hidden  max-h-[320px] shadow ring-1 ring-black ring-opacity-5 sm:rounded-lg overflow-y-auto">
+            <table className="min-w-full divide-y divide-gray-300">
+              <thead className="bg-gray-50">
+                <tr>
+                  <th
+                    scope="col"
+                    className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3"
+                  >
+                    Description
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Amount
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Category
+                  </th>
+                  <th
+                    scope="col"
+                    className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                  >
+                    Action
+                  </th>
+                </tr>
+              </thead>
+              <tbody className="bg-white">
+                {expenses.map((exp) => (
+                  <tr key={exp.id} className="even:bg-gray-50">
+                    <td className="whitespace-nowrap py-4 pl-4 pr-3 text-sm font-medium text-gray-900 sm:pl-3">
+                      {exp.description}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {exp.amount}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      {exp.category}
+                    </td>
+                    <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                      <button className="px-3 py-1 bg-red-400 text-white rounded">
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                ))}
+              </tbody>
+              <tfoot className="bg-gray-50">
+                <td className="py-3.5 pl-4 pr-3 text-left text-sm font-semibold text-gray-900 sm:pl-3">
+                  Total
+                </td>
+                <td className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"></td>
+                <td className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"></td>
+                <td className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"></td>
+              </tfoot>
+            </table>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ExpenseList;
